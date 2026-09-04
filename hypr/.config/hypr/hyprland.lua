f8791184f437658e879db429128bdfe92b78d6b0
Laurent Lao <lao.laurent@gmail.com>
2026-09-04 12:02:04 -0400

    Add hyprsunset to autorun

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -65,6 +65,7 @@ local browser = "firefox"
    hl.exec_cmd(terminal)
    hl.exec_cmd("nm-applet")
    hl.exec_cmd("hypridle")
+   hl.exec_cmd("hyprsunset")
    hl.exec_cmd("waybar & awww-daemon")
  end)
 
